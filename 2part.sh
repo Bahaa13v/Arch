d6909ee6c222e0d041a6bafc07b60080d1665ccb
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2024-08-05 12:09:55 +0100

    Update 2part.sh

--- 2part.sh
+++ 2part.sh
@@ -1,25 +1,2 @@
 #!/bin/bash
-
-
-DISK="/dev/sda"
-PART1_SIZE=$(($(lsblk -b -o SIZE -n -l $DISK | awk '{print $1}') - 16*1024*1024*1024))
-PART2_SIZE="16G"
-
-
-
-fdisk $DISK <<EOF
-o
-n
-p
-1
-2048
-
-+${PART1_SIZE}
-n
-p
-2
-
-
-+${PART2_SIZE}
-w
-EOF
+echo -e "n\np\n1\n\n-16G\nn\np\n2\n\n\nw" | fdisk /dev/sda
