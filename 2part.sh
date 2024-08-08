12758be2192d854f0d9895e6597113337738bfc7
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2024-08-08 08:50:04 +0100

    Update 2part.sh

--- 2part.sh
+++ 2part.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-echo -e "o\nw" | fdisk /dev/sda
-wipefs -a /dev/sda
-echo -e "n\np\n1\n\n-16G\ny\nn\np\n2\n\ny\nw" | fdisk /dev/sda
+sudo dd if=/dev/zero of=/dev/sda bs=1M status=progress
+echo -e "o\nn\np\n1\n\n-16G\ny\nn\np\n2\n\ny\nw" | fdisk /dev/sda
