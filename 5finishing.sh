70be4011aa62e4cc0ec44a1a8633a55652c4d26e
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2024-08-08 09:50:32 +0100

    Update 5finishing.sh

--- 5finishing.sh
+++ 5finishing.sh
@@ -1,6 +1,10 @@
-arch-chroot /mnt/problem.sh
+arch-chroot /mnt <<EOF
+bash problem.sh
+exit
+EOF
 
 umount -R /mnt
+swapoff /dev/sda2
 
 echo "Done installing thanks to bahaa"
 echo "type the word reboot"
