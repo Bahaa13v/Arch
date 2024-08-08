df3f3bebde540c0b025d9745563b9db757519da0
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2024-08-08 09:44:42 +0100

    Update Arch.sh

--- Arch.sh
+++ Arch.sh
@@ -1,23 +1,23 @@
 #!bin/bash
 
 echo "Bahaa's Arch Project..."
-
+sleep 3
+bash 1setup.sh
+echo "Done Setup"
 sleep 3
 
-bash 1setup.sh
 
-echo "Done Setup"
+#bash 2part.sh
+echo "CANCEL IF YOU DIDN'T PARTITION!!!"
+sleep 3
 
-bash 2part.sh
-
-echo "Done Part"
 
 bash 3install.sh
-
 echo "Done Installing"
+sleep 3
 
 bash 4configs.sh
-
 echo "Done Configuring"
+sleep 3
 
 bash 5finishing.sh
