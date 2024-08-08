dc02971a8bb0a6ca62f8bcee5b3603dfafcbee71
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2024-08-08 15:20:29 +0100

    Update 5finishing.sh

--- 5finishing.sh
+++ 5finishing.sh
@@ -10,13 +10,3 @@ swapoff /dev/sda2
 
 echo "Done installing thanks to bahaa"
 echo "when you reboot and log in type i then enter"
-sleep 3
-read -p "Do you want to reboot? (y/n): " answer
-
-# Check the user's response
-if [ "$answer" = "y" ]; then
-    echo "Rebooting..."
-    reboot
-else
-    echo "if there is a proglem you should check it yourself and if you wanna edit anything feel free"
-fi
