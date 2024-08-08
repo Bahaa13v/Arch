f36d7d265ea19b96301e025bbde0cee9796b7efa
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2024-08-08 08:51:06 +0100

    Update 3install.sh

--- 3install.sh
+++ 3install.sh
@@ -6,6 +6,6 @@ swapon /dev/sda2
 
 mount /dev/sda1 /mnt
 
-pacstrap -K /mnt base linux linux-firmware grub sudo networkmanager nano
+pacstrap -K /mnt base linux linux-firmware
 
 genfstab -U /mnt >> /mnt/etc/fstab
