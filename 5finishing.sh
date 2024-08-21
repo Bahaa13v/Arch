a1b8bb87c97939e567214e20db49c2232366dcf3
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2024-08-21 21:18:29 +0100

    Update 5finishing.sh

--- 5finishing.sh
+++ 5finishing.sh
@@ -2,12 +2,10 @@
 
 arch-chroot /mnt <<EOF
 bash problem.sh
-rm problem.sh
 exit
 EOF
 
+rm /mnt/problem.sh
+
 umount -R /mnt
 swapoff /dev/sda2
-
-echo "Done installing thanks to bahaa"
-echo "when you reboot and log in type i then enter"
