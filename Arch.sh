9b97068d39624c6b327ec77572ab9c5ccbeceafe
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2024-08-21 21:17:32 +0100

    Update Arch.sh

--- Arch.sh
+++ Arch.sh
@@ -1,33 +1,26 @@
 #!/bin/bash
 
+clear
 echo "Bahaa's Arch Project..."
 sleep 3
 bash 1setup.sh
 echo "Done Setup"
 sleep 3
 
-
+clear
 bash 2part.sh
 echo "Deno Partitioning"
 sleep 3
 
-
+clear
 bash 3install.sh
 echo "Done Installing"
 sleep 3
 
+clear
 bash 4configs.sh
 echo "Done Configuring"
 sleep 3
 
+clear
 bash 5finishing.sh
-
-sleep 3
-read -p "Do you want to reboot? (y/n): " answer
-
-if [ "$answer" = "y" ]; then
-    echo "Rebooting..."
-    reboot
-else
-    echo "if there is a proglem you should check it yourself and if you wanna edit anything feel free"
-fi
