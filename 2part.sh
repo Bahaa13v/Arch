07b6497036a629904a21366a953763b4406644e4
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2024-08-23 15:48:44 +0100

    Update 2part.sh

--- 2part.sh
+++ 2part.sh
@@ -7,3 +7,10 @@ parted /dev/sda --script -- mkpart primary btrfs 0% -16GB
 parted /dev/sda --script -- mkpart primary linux-swap -16GB 100%
 
 partprobe /dev/sda
+
+swapon /dev/sda2
+mount /dev/sda1 /mnt
+
+lsblk
+echo "if this isn't correct cancel"
+sleep 5
