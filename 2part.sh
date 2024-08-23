2893276036ce61219a8a80de5ac57a1a61f69f75
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2024-08-23 15:47:38 +0100

    Update 2part.sh

--- 2part.sh
+++ 2part.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 wipefs --all --force /dev/sda
+
 parted /dev/sda --script -- mklabel msdos
-parted /dev/sda --script -- mkpart primary 0% -16GiB mkpart primary -16GiB 100%
+parted /dev/sda --script -- mkpart primary btrfs 0% -16GB
+parted /dev/sda --script -- mkpart primary linux-swap -16GB 100%
+
+partprobe /dev/sda
