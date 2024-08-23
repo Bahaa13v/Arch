18e0348cc1b4869fd14138b3d66c4f1c621ec7d9
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2024-08-23 15:48:25 +0100

    Update 3install.sh

--- 3install.sh
+++ 3install.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-swapon /dev/sda2
-mount /dev/sda1 /mnt
-
-lsblk
-echo "if this isn't correct cancel"
-sleep 5
-
 pacstrap -K /mnt base linux linux-firmware
 
 genfstab -U /mnt >> /mnt/etc/fstab
